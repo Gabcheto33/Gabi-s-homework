af27089c8a831254f87867c4f47eee71784ab89c
Gabcheto33 <113976407+Gabcheto33@users.noreply.github.com>
2024-01-21 21:00:45 +0200

    Add files via upload

---
+++ Untitled-1.cs
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
+    <p>My first paragraph.</p>
+    <p>This is a paragraph.</p>
+    <p>This is another paragraph.</p>
+    <p>
+        <strong>This text is bold</strong>
+    </p>
+    <p style="font-style: italic">This text is italic</p>
+    <p>This is <sub>subscript</sub> and <sup>superscript</sup></p>
+    <p> Here is a quote from WWF's website:</p>
+    <p>
+             For 60 years, WWF's has worked to help people and nature thrive. As the world's leading conservation organization WWF's works in nearly 100 countries.
+             At every level, we collaborate with people around the world to develop and deliver innovative solutions  that protect communities, wildfire, and the places in which they live.
+    </p>
+    <p>WWF's goal is to: "Build a future where people live in harmony with nature."</p>
+</html>
+
+
+
